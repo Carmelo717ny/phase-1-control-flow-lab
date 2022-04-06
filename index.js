fd51bc5386ee39b4046c8db4e8775fcae7f953fe
Carmelo Martinez-Vazquez <Carmelo.M.Vazquez@hotmail.com>
2022-04-05 22:03:28 -0400

    commit tree 9c25b94c7e885030dab2141560e573ad63ff669f

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+if (distanceInFeet <= 400) {
+  return 'This one is on me!';
+} else if (distanceInFeet <= 2000) {
+  return 'That will be twenty bucks.'; 
+} else if (distanceInFeet <= 2500) {
+  return 'I will gladly take your thirty bucks.';
+} else {
+  return 'No can do.'; }
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(NYC){
   // Write your code here!
+  if (NYC) {
+return "Ok, sounds good." ; }
+else (NYC) => {
+return "No go."; }
 }
 
 function switchOnCharmFromTip(){
